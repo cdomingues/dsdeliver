d8a1f6e8ed1c0d81bd5ab11a1bf4454311356d06
Carlos Domingues <domingues_santos@hotmail.com>
2021-01-26 15:56:03 -0300

    criando front web

---
+++ front-web/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Hello from './Hello';
+//import Counter from './counter';
+
+function App() {
+  return (
+    <div >
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ front-web/src/Hello.tsx
@@ -0,0 +1,7 @@
+function Hello(){
+    return(
+        <h1>Hello testando!!</h1>
+    )
+}
+
+export default Hello;
